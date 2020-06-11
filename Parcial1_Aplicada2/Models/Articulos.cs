37be3d0faf4164c673e2ce834e4c3386ef49a3d0
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-06-10 20:49:53 -0400

    Creando Modelo

---
+++ Parcial1_Aplicada2/Models/Articulos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Aplicada2.Models
+{
+    public class Articulos
+    {
+        [Key]
+        [Required(ErrorMessage = "No debe de estar Vacío el campo 'ArticuloId'")]
+        public int ArticuloId { get; set; }
+
+        [Required(ErrorMessage = "No debe de estar Vacío el campo 'Descripción'")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "No debe de estar Vacío el campo 'Existencia'")]
+        public int Existencia { get; set; }
+
+        [Required(ErrorMessage = "No debe de estar Vacío el campo 'Costo'")]
+        public decimal Costo { get; set; }
+
+        [Required(ErrorMessage = "No debe de estar Vacío el campo 'Valor de Inventario'")]
+        public int ValorInventario { get; set; }
+
+        public Articulos()
+        {
+            ArticuloId = 0;
+            Descripcion = string.Empty;
+            Existencia = 0;
+            Costo = 0;
+            ValorInventario = 0;
+        }
+
+        public Articulos(int articuloId, string descripcion, int existencia, decimal costo, int valorInventario)
+        {
+            ArticuloId = articuloId;
+            Descripcion = descripcion;
+            Existencia = existencia;
+            Costo = costo;
+            ValorInventario = valorInventario;
+        }
+    }
+}
