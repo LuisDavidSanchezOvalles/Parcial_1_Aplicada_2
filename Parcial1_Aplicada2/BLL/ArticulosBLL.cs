c7c45c93d5e06f3495f4b76989586834d778fb55
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-06-10 21:10:06 -0400

    Creacion de la BLL

--- Parcial1_Aplicada2/BLL/ArticulosBLL.cs
+++ Parcial1_Aplicada2/BLL/ArticulosBLL.cs
@@ -1,12 +1,169 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Parcial1_Aplicada2.DAL;
+using Parcial1_Aplicada2.Models;
 
 namespace Parcial1_Aplicada2.BLL
 {
     public class ArticulosBLL
     {
+        public static bool Existe(int id)
+        {
+            Contexto db = new Contexto();
+            bool encontrado = false;
 
+            try
+            {
+                encontrado = db.Articulos.Any(a => a.ArticuloId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return encontrado;
+        }
+
+        public static bool Insertar(Articulos articulo)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                db.Articulos.Add(articulo);
+                paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(Articulos articulo)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                db.Entry(articulo).State = EntityState.Modified;
+                paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Guardar(Articulos articulo)
+        {
+            if (!Existe(articulo.ArticuloId))
+                return Insertar(articulo);
+            else
+                return Modificar(articulo);
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                var articulo = db.Articulos.Find(id);
+
+                if (articulo != null)
+                {
+                    db.Articulos.Remove(articulo);
+                    paso = db.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static Articulos Buscar(int id)
+        {
+            Contexto db = new Contexto();
+            Articulos articulo;
+
+            try
+            {
+                articulo = db.Articulos.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return articulo;
+        }
+
+        public static List<Articulos> GetArticulos()
+        {
+            List<Articulos> lista = new List<Articulos>();
+            Contexto db = new Contexto();
+            try
+            {
+                lista = db.Articulos.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return lista;
+        }
+
+        public static List<Articulos> GetList(Expression<Func<Articulos, bool>> criterio)
+        {
+            List<Articulos> lista = new List<Articulos>();
+            Contexto db = new Contexto();
+            try
+            {
+                lista = db.Articulos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return lista;
+        }
     }
 }
