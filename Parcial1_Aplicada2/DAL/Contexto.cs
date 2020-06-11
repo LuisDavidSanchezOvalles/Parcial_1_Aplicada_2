2a7120f51998262f4419229d900e5274b0b1dcf0
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-06-10 20:53:32 -0400

    Creando el Contexto

--- Parcial1_Aplicada2/DAL/Contexto.cs
+++ Parcial1_Aplicada2/DAL/Contexto.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Parcial1_Aplicada2.Models;
 
 namespace Parcial1_Aplicada2.DAL
 {
     public class Contexto : DbContext
     {
+        public DbSet<Articulos> Articulos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data Source = C:\Users\luisd\OneDrive\Escritorio\Universidad\Programacion Aplicada 2\Base_de_Datos_de_Blazor\Parcial1DB\Parcial1.db");
